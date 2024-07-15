fa34c79dec543fbc96cf544f4574b705c291342d
disha-navsoft <dishaarora1996@gmail.com>
2024-07-15 22:35:07 +0530

    changes in database config

--- scripts/install_database.sh
+++ scripts/install_database.sh
@@ -11,13 +11,13 @@ sudo systemctl enable postgresql
 # Switch to the postgres user and create a new database
 sudo -i -u postgres psql -- << EOF
 -- Create a new database
-CREATE DATABASE foodOnline;
+CREATE DATABASE foodonline;
 
 -- Optionally, create a new user with a password and grant all privileges on the new database
--- CREATE USER myuser WITH PASSWORD 'mypassword';
--- GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;
+CREATE USER myuser WITH PASSWORD '1234';
+GRANT ALL PRIVILEGES ON DATABASE foodonline TO myuser;
 
 \q
 EOF
 
-echo "PostgreSQL installed and database 'mydatabase' created successfully."
+echo "PostgreSQL installed and database 'foodonline' created successfully."
